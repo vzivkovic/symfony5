0318ff3fff0696c201da656e1a7db0cd9c0e0255
vzivkovic <vzivkovic@me.com>
2020-07-18 07:43:36 +0200

    Add tailwind and basic template

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+let tailwindcss = require('tailwindcss');
+
+module.exports = {
+    plugins: [
+        tailwindcss('./tailwind.config.js'), // your tailwind.js configuration file path
+        require('autoprefixer'),
+        require('postcss-import')
+    ]
+};
+
+// require('postcss-import')
